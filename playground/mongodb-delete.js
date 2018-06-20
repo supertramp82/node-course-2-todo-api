484b882cce741b1d94bbb355b875a322fd8aff2e
Oleg Khryashchev <oleg.khryashchev@rating.nocal.quadinfo.com>
2018-06-20 12:35:06 -0700

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,20 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect(
+  'mongodb:..localhost:27017/TodoApp',
+  (err, db) => {
+    if (err) {
+      return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    db.collection('Todos')
+      .findOneAndDelete({ text: 'Something to do 3' })
+      .then(result => {
+        console.log(result);
+      });
+
+    //db.close();
+  }
+);
