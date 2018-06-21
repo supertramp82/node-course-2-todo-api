0109823acebc89f3c5fdd91530a32508e44bb49d
Oleg Khryashchev <oleg.khryashchev@rating.nocal.quadinfo.com>
2018-06-20 17:54:59 -0700

    Add FET /todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,17 @@ app.post('/todos', (req, res) => {
   console.log(req.body);
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 app.listen(3000, () => {
   console.log('Listening to port 3000');
 });
